61550d5fc3f92bfab231b62974068567238a48a0
Liss McCabe <liss@eristiccode.com>
2016-02-25 16:19:42 -0800

    Find fields containing years

--- lib/el_answers.ex
+++ lib/el_answers.ex
@@ -9,8 +9,7 @@ defmodule ElAnswers do
   end
 
   @doc """
-  Given a collection, lists the items with a type of :cd with a running time
-  over an hour.
+  Given a collection, lists the items with a type of :cd with a running time over an hour.
   """
   def cds_with_hour_plus_running_times(coll) do
     ElCollection.by_type(coll) |>
@@ -32,6 +31,39 @@ defmodule ElAnswers do
   contains a probable year.
   """
   def items_containing_years(collection) do
+    Enum.filter(collection, &(match_fields(&1)))
+  end
+
+  defp match_fields(record) do
+    match_title(record) or
+    match_chapters(record) or
+    match_tracks(record)
+  end
+
+  def match_title(record) do
+    string_contains_year?(Map.get(record, :title))
+  end
+
+  def match_chapters(record) do
+    Map.get(record, :chapters, [""]) |>
+    Enum.any?(&(string_contains_year?(&1)))
+  end
+
+  def match_tracks(record) do
+    if Map.has_key?(record, :tracks) do
+      Map.get(record, :tracks) |>
+      Enum.any?(fn(record) ->
+        Map.fetch!(record, :name) |>
+        string_contains_year?
+      end)
+    end
+  end
+
+  @doc """
+  Naive date regex. Assumes that all positive 4 digit numbers are years.
+  """
+  def string_contains_year?(string) do
+    Regex.match?(~r/\b\d{4}\b/, string)
   end
 
   defp cd_length(cd_map) do
--- test/el_answers_test.exs
+++ test/el_answers_test.exs
@@ -45,4 +45,29 @@ defmodule ElAnswersTest do
 
     assert ElAnswers.authors_with_cds(coll) == expected
   end
+
+  test "items_contaning_years/1 returns correctly with mock data" do
+    coll = InputMap.data('test/years_in_titles.json')
+    expected = [
+      %{author: "mary",
+        chapters: ["Journal from 1987", "two", "three"],
+        price: 15.99,
+        title: "Journals",
+        type: "book",
+        year: 1999},
+      %{author: "μ's",
+        price: 20.99,
+        title: "Love Live!",
+        tracks: [%{name: "Bokura wa Ima no Naka de (Live 2013)", seconds: 180}, %{name: "two", seconds: 200}],
+        type: "cd",
+        year: 2000},
+      %{author: "nagisa",
+        price: 18.99,
+        title: "Cheeses I Came Back For",
+        tracks: [%{name: "one", seconds: 180}, %{name: "Mascarpone in 2014", seconds: 200}],
+        type: "cd",
+        year: 2000}]
+
+    assert ElAnswers.items_containing_years(coll) == expected
+  end
 end
